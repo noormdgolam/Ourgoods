59a04259eda681d27effb4d147a6f067399d1415
MDGOLAMNOOR <noormdgolam@gmail.com>
2026-07-16 13:22:16 +0600

    Deploy: Built static files to root directory for cPanel git deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
   plugins: [react()],
+  build: {
+    outDir: '../',
+    emptyOutDir: false,
+  },
   server: {
     host: true,
     watch: {
